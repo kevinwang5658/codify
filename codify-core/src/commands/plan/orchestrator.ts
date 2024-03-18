d51bec8435321e310c735968f8e969eb4dbe1e99
kevinwang <kevinwang5658@gmail.com>
2024-03-18 14:35:55 -0400

    Fixed oclif not working with ESM

--- codify-core/src/commands/plan/orchestrator.ts
+++ codify-core/src/commands/plan/orchestrator.ts
@@ -1,18 +1,17 @@
-import { ConfigCompiler } from '../../config-compiler/index.js';
-import { PluginCollection } from '../../plugins/plugin-collection.js';
-
 export const PlanOrchestrator = {
   async run(rootDirectory: string): Promise<string> {
-    const parsedProject = await ConfigCompiler.parseProject(rootDirectory);
+    // const parsedProject = await ConfigCompiler.parseProject(rootDirectory);
+    //
+    // const pluginCollection = await PluginCollection.create(parsedProject);
+    // const definitions = await pluginCollection.getResourceDefinitions();
+    //
+    // const compiledProject = await ConfigCompiler.compileProject(parsedProject, definitions);
+    // console.log(compiledProject);
+    // const plan = await pluginCollection.getPlan(compiledProject);
+    //
+    // await pluginCollection.destroy();
+    // return JSON.stringify(plan, null, 2);
 
-    const pluginCollection = await PluginCollection.create(parsedProject);
-    const definitions = await pluginCollection.getResourceDefinitions();
-
-    const compiledProject = await ConfigCompiler.compileProject(parsedProject, definitions);
-    console.log(compiledProject);
-    const plan = await pluginCollection.getPlan(compiledProject);
-
-    await pluginCollection.destroy();
-    return JSON.stringify(plan, null, 2);
+    return '';
   },
 };
